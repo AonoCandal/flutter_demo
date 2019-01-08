d1b2777e9d53a6ddaf20c28e41723e032ea68345
LiJianGe <jiange.li@56qq.com>
2019-01-08 16:43:40 +0800

    完善加载

--- lib/page/progress_dialog_page.dart
+++ lib/page/progress_dialog_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/widget/progress_dialog.dart';
@@ -10,50 +11,72 @@ class ProgressDialogPage extends StatefulWidget {
   }
 }
 
-class ProgressDialogState extends State<ProgressDialogPage>
-    with TickerProviderStateMixin {
+class ProgressDialogState extends State<ProgressDialogPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('加载弹框'),
+        title: Text('加载弹窗 & 状态弹窗'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            buildButton('加载2秒钟, 弹出成功文案', _click2SecondSuccess),
-            buildButton('加载2秒钟, 弹出失败文案', _click2SecondFailed),
-          ],
-        ),
+      body: ListView(
+        children: [
+          _listItem(
+              '加载2秒钟',
+                  () => ProgressDialog.showProgressDialog(
+                  context: context,
+                  future: Future.delayed(Duration(seconds: 2)))),
+          _listItem('弹出成功状态2秒钟',
+                  () => ProgressDialog.showSuccessDialog(context: context)),
+          _listItem(
+              '弹出自定义状态弹框',
+                  () => ProgressDialog.showStateDialog(
+                  context: context,
+                  loadingText: '警告',
+                  icon: Icon(
+                    Icons.error,
+                    size: kDefaultIconSize,
+                    color: Colors.white,
+                  ))),
+          _listItem(
+              '加载2秒钟之后弹出成功',
+                  () => ProgressDialog.showProgressDialog(
+                context: context,
+                future: Future.delayed(Duration(seconds: 2)).then((value) {
+                  ProgressDialog.showSuccessDialog(context: context);
+                }),
+              )),
+          _listItem(
+              '加载2秒钟之后弹出失败',
+                  () => ProgressDialog.showProgressDialog(
+                context: context,
+                future: Future.delayed(Duration(seconds: 2)).then((value) {
+                  ProgressDialog.showFailDialog(context: context);
+                }),
+              )),
+          _listItem(
+              '异步加载并随机捕获错误(仿实际使用)',
+                  () => ProgressDialog.showProgressDialog(
+                  context: context,
+                  future: Future.delayed(Duration(seconds: 2), () {
+                    Random r = Random();
+                    if (!r.nextBool()) {
+                      throw Exception();
+                    }
+                  }).then((value) {
+                    ProgressDialog.showSuccessDialog(context: context);
+                  }).catchError((e) {
+                    ProgressDialog.showFailDialog(context: context);
+                  }))),
+        ],
       ),
     );
   }
 
-  Widget buildButton(String name, VoidCallback onclick) {
-    return RaisedButton(
-      onPressed: onclick,
-      child: Text(name),
+  Widget _listItem(String title, VoidCallback listener) {
+    return ListTile(
+      title: Text(title),
+      trailing: Icon(Icons.arrow_forward_ios),
+      onTap: listener,
     );
-  }
-
-  /// 加载2秒钟, 弹出成功文案
-  void _click2SecondSuccess() {
-    ProgressDialog.showProgressDialog(
-        context: context, future: Future.delayed(Duration(seconds: 2)));
-  }
-
-  /// 加载2秒钟, 弹出失败Toast
-  void _click2SecondFailed() {
-    ProgressDialog.showStateDialog(
-        context: context,
-        loadingText: '加载成功',
-        icon: Icon(
-          Icons.check_circle_outline,
-          size: 28.0,
-          color: Colors.white,
-        ));
   }
 }
--- lib/widget/progress_dialog.dart
+++ lib/widget/progress_dialog.dart
@@ -9,21 +9,31 @@ const int _kTickCount = 8;
 const int _kHalfTickCount = _kTickCount ~/ 2;
 const Color _kTickColor = CupertinoColors.lightBackgroundGray;
 const Color _kActiveTickColor = Color(0xFF9D9D9D);
-const Color _kDefaultBgColor = Color(0xE6333333);
 const double _kDefaultIndicatorRadius = 10.0;
-const double _kDefaultIconSize = 28.0;
+
+/// 默认弹窗背景颜色 灰
+const Color _kDefaultBgColor = Color(0xE6333333);
+
+/// 默认Icon大小
+const double kDefaultIconSize = 28.0;
+
+/// 默认弹窗边长
 const double _kDefaultSideLength = 91.0;
+
+/// 默认加载文案
 const String _kDefaultLoadingText = '正在加载';
+
+/// 默认加载文案style
 const TextStyle _kDefaultLoadingTextStyle =
-    TextStyle(color: Colors.white, fontSize: 15.0);
+TextStyle(color: Colors.white, fontSize: 15.0);
 
 class ProgressDialog {
   /// 显示正在加载弹窗
   static showProgressDialog({
     @required BuildContext context,
-    @required Future future,
-    String loadingText = _kDefaultLoadingText,
-    TextStyle loadingTextStyle = _kDefaultLoadingTextStyle,
+    @required Future future, // 可执行的异步事件处理 (Futura处理完毕后自动退出弹窗)
+    String loadingText = _kDefaultLoadingText, // 加载文案
+    TextStyle loadingTextStyle = _kDefaultLoadingTextStyle, // 加载文案style
   }) {
     showStateDialog(
         context: context,
@@ -33,57 +43,87 @@ class ProgressDialog {
         loadingTextStyle: loadingTextStyle);
   }
 
+  static showSuccessDialog({
+    @required BuildContext context,
+    String loadingText = '加载成功',
+  }) {
+    showStateDialog(
+        context: context,
+        loadingText: loadingText,
+        icon: Icon(
+          Icons.check_circle_outline,
+          size: kDefaultIconSize,
+          color: Colors.white,
+        ));
+  }
+
+  static showFailDialog({
+    @required BuildContext context,
+    String loadingText = '加载失败',
+  }) {
+    showStateDialog(
+        context: context,
+        loadingText: loadingText,
+        icon: Icon(
+          Icons.highlight_off,
+          size: kDefaultIconSize,
+          color: Colors.white,
+        ));
+  }
+
   /// 显示状态弹窗
   static showStateDialog({
     @required BuildContext context,
-    Future future,
-    Color bgColor = _kDefaultBgColor,
-    String loadingText = _kDefaultLoadingText,
-    TextStyle loadingTextStyle = _kDefaultLoadingTextStyle,
-    double iconSize = _kDefaultIconSize,
-    Widget icon,
-    double sideLength = _kDefaultSideLength,
-    bool barrierDismissible = true,
+    Future future, // 可执行的异步事件处理 (Futura处理完毕后自动退出弹窗)
+    Color bgColor = _kDefaultBgColor, // 弹窗的背景颜色
+    String loadingText = _kDefaultLoadingText, // 加载文案
+    TextStyle loadingTextStyle = _kDefaultLoadingTextStyle, // 加载文案style
+    double iconSize = kDefaultIconSize, // 图标大小
+    Widget icon, // 图标 (默认为菊花)
+    double sideLength = _kDefaultSideLength, // 弹框边长
+    bool barrierDismissible = true, // 点击空白区域是否消失
+    Color barrierColor, // 阴影颜色
   }) {
     // 弹窗是否已经弹出过
     bool hasPopped = false;
-    showGeneralDialog(
-        context: context,
-        barrierDismissible: barrierDismissible,
-        barrierLabel:
-            MaterialLocalizations.of(context).modalBarrierDismissLabel,
-        transitionDuration: const Duration(milliseconds: 150),
-        transitionBuilder: _buildMaterialDialogTransitions,
-        pageBuilder: (BuildContext buildContext, Animation<double> animation,
-            Animation<double> secondaryAnimation) {
-          final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
-          final Widget pageChild = Builder(builder: (BuildContext context) {
-            return ProgressCenterContent(
-              bgColor: bgColor,
-              loadingText: loadingText,
-              loadingTextStyle: loadingTextStyle,
-              iconSize: iconSize,
-              icon: icon,
-              sideLength: sideLength,
-            );
-          });
-          return WillPopScope(
-            onWillPop: () {
-              hasPopped = true;
-              return Future.value(true);
-            },
-            child: SafeArea(
-              child: Builder(builder: (BuildContext context) {
-                return theme != null
-                    ? Theme(data: theme, child: pageChild)
-                    : pageChild;
-              }),
-            ),
+    Route route = _DialogRoute(
+      barrierDismissible: barrierDismissible,
+      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+      barrierColor: barrierColor,
+      transitionDuration: const Duration(milliseconds: 150),
+      transitionBuilder: _buildMaterialDialogTransitions,
+      pageBuilder: (BuildContext buildContext, Animation<double> animation,
+          Animation<double> secondaryAnimation) {
+        final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
+        final Widget pageChild = Builder(builder: (BuildContext context) {
+          return ProgressCenterContent(
+            bgColor: bgColor,
+            loadingText: loadingText,
+            loadingTextStyle: loadingTextStyle,
+            iconSize: iconSize,
+            icon: icon,
+            sideLength: sideLength,
           );
         });
+        return WillPopScope(
+          onWillPop: () {
+            hasPopped = true;
+            return Future.value(true);
+          },
+          child: SafeArea(
+            child: Builder(builder: (BuildContext context) {
+              return theme != null
+                  ? Theme(data: theme, child: pageChild)
+                  : pageChild;
+            }),
+          ),
+        );
+      },
+    );
+    Navigator.of(context, rootNavigator: true).push(route);
     (future ?? Future.delayed(Duration(seconds: 2))).whenComplete(() {
       if (!hasPopped) {
-        Navigator.of(context).pop();
+        Navigator.of(context).removeRoute(route);
       }
     });
   }
@@ -165,11 +205,11 @@ class ProgressCenterContentState extends State<ProgressCenterContent>
               width: widget.iconSize,
               child: widget.icon == null
                   ? CustomPaint(
-                      painter: _CupertinoActivityIndicatorPainter(
-                        position: _controller,
-                        radius: widget.iconSize / 2,
-                      ),
-                    )
+                painter: _CupertinoActivityIndicatorPainter(
+                  position: _controller,
+                  radius: widget.iconSize / 2,
+                ),
+              )
                   : widget.icon,
             ),
             Padding(
@@ -189,17 +229,18 @@ class ProgressCenterContentState extends State<ProgressCenterContent>
   }
 }
 
+/// 菊花画笔
 class _CupertinoActivityIndicatorPainter extends CustomPainter {
   _CupertinoActivityIndicatorPainter({
     this.position,
     double radius,
   })  : tickFundamentalRRect = RRect.fromLTRBXY(
-            -radius,
-            1.0 * radius / _kDefaultIndicatorRadius,
-            -radius / 2.0,
-            -1.0 * radius / _kDefaultIndicatorRadius,
-            1.0,
-            1.0),
+      -radius,
+      1.0 * radius / _kDefaultIndicatorRadius,
+      -radius / 2.0,
+      -1.0 * radius / _kDefaultIndicatorRadius,
+      1.0,
+      1.0),
         super(repaint: position);
 
   final Animation<double> position;
@@ -216,7 +257,7 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
 
     for (int i = 0; i < _kTickCount; ++i) {
       final double t =
-          (((i + activeTick) % _kTickCount) / _kHalfTickCount).clamp(0.0, 1.0);
+      (((i + activeTick) % _kTickCount) / _kHalfTickCount).clamp(0.0, 1.0);
       paint.color = Color.lerp(_kActiveTickColor, _kTickColor, t);
       canvas.drawRRect(tickFundamentalRRect, paint);
       canvas.rotate(-_kTwoPI / _kTickCount);
@@ -228,5 +269,69 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
   @override
   bool shouldRepaint(_CupertinoActivityIndicatorPainter oldPainter) {
     return oldPainter.position != position;
+  }
+}
+
+/// 弹窗Route
+class _DialogRoute<T> extends PopupRoute<T> {
+  _DialogRoute({
+    @required RoutePageBuilder pageBuilder,
+    bool barrierDismissible = true,
+    String barrierLabel,
+    Color barrierColor = const Color(0x80000000),
+    Duration transitionDuration = const Duration(milliseconds: 200),
+    RouteTransitionsBuilder transitionBuilder,
+    RouteSettings settings,
+  })  : assert(barrierDismissible != null),
+        _pageBuilder = pageBuilder,
+        _barrierDismissible = barrierDismissible,
+        _barrierLabel = barrierLabel,
+        _barrierColor = barrierColor,
+        _transitionDuration = transitionDuration,
+        _transitionBuilder = transitionBuilder,
+        super(settings: settings);
+
+  final RoutePageBuilder _pageBuilder;
+
+  @override
+  bool get barrierDismissible => _barrierDismissible;
+  final bool _barrierDismissible;
+
+  @override
+  String get barrierLabel => _barrierLabel;
+  final String _barrierLabel;
+
+  @override
+  Color get barrierColor => _barrierColor;
+  final Color _barrierColor;
+
+  @override
+  Duration get transitionDuration => _transitionDuration;
+  final Duration _transitionDuration;
+
+  final RouteTransitionsBuilder _transitionBuilder;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return Semantics(
+      child: _pageBuilder(context, animation, secondaryAnimation),
+      scopesRoute: true,
+      explicitChildNodes: true,
+    );
+  }
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (_transitionBuilder == null) {
+      return FadeTransition(
+          opacity: CurvedAnimation(
+            parent: animation,
+            curve: Curves.linear,
+          ),
+          child: child);
+    } // Some default transition
+    return _transitionBuilder(context, animation, secondaryAnimation, child);
   }
 }
